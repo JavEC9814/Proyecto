fe8c78cc4f4e70a2f790880936d8582130a0d5e7
JavEC9814 <jecedeno@ucompensar.edu.co>
2021-10-01 01:11:38 -0500

    1/10/2021

---
+++ assets/js/funciones.js
@@ -0,0 +1,6 @@
+function getData(){
+    var objName=document.getElementById("nombre");
+    var objCode=document.getElementById("nombre");
+    console.log(objName.value);
+    console.log(objCode.value);
+}
--- bd/database.sql
+++ bd/database.sql
@@ -1,1 +1,2 @@
 INSERT INTO `usuario` (`id_usuario`, `tipo_usuario`, `nombre_usuario`, `contraseña`) VALUES ('0001', 'Enfermera', 'Paula Ramirez', '123456');
+SELECT * FROM `usuario`;
