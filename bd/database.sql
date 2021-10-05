96dc8729dd65687bf561f661aace0d100cdcbe35
JavEC9814 <jecedeno@ucompensar.edu.co>
2021-10-05 13:29:10 -0500

    5/10/2021 Javier

--- bd/database.sql
+++ bd/database.sql
@@ -1,2 +1,7 @@
-INSERT INTO `usuario` (`id_usuario`, `tipo_usuario`, `nombre_usuario`, `contraseña`) VALUES ('0001', 'Enfermera', 'Paula Ramirez', '123456');
-SELECT * FROM `usuario`;
+/*INSERT INTO `usuario` (`id_usuario`, `tipo_usuario`, `nombre_usuario`, `contraseña`) VALUES ('0001', 'Enfermera', 'Paula Ramirez', '123456');
+SELECT * FROM `usuario`;*/
+
+conexion = psycopg2.connect(database="Solmedi", user="Javier Cedeño", password="3144533398", host="127.0.0.1", port="5432")
+print("BD conectada")
+
+publicg
